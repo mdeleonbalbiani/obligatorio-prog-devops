415cf86a15f7536e7550e42dad06f8dc3aba33ff
mdeleonbalbiani <martinadeleonbalbiani@gmail.com>
2025-11-12 22:42:26 -0300

    Corrección de errores en validación de primer parametro

--- bash/ej1_crea_usuarios.sh
+++ bash/ej1_crea_usuarios.sh
@@ -10,19 +10,22 @@ archivo="${!total_params}"
 
 #Validar que haya al menos un parametro
 if [ $# -eq 0 ]; then
-    echo "Error: No se proporciono ningun argumento, se necesitan al menos el archivo que contenga los usuarios" 2>&1
+    echo "Error: No se proporciono ningun argumento, se necesitan al menos el archivo que contenga los usuarios" >&2
     exit 1
 fi
 
 #Validación primer parametro
-if [ $1 = "-i" ]; then
+if [ "$1" = "-i" ]; then
     modo_info=true
+elif [ "$1" = "-c" ]; then
+    #Validación que contraseña no sea vacía
+    if [ -z "$2" ]; then
+        echo "Error: Falta la contraseña después de -c." >&2
+        exit 2
+    fi
+    password="$2"
 else
-    if [ $1 = "-c" ]; then
-        password=$2
-    else
-        archivo=$1
-    fi 
+    archivo="$1"
 fi
 
 #Validación segundo parametro
