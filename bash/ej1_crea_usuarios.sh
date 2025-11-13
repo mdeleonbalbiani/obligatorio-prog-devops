483ad5182defee472005a500d548862291df2f1f
mdeleonbalbiani <martinadeleonbalbiani@gmail.com>
2025-11-12 22:14:51 -0300

    Validación de que contenga paramentros

--- bash/ej1_crea_usuarios.sh
+++ bash/ej1_crea_usuarios.sh
@@ -5,3 +5,8 @@ modo_info=false
 password=""
 archivo=""
 
+#Validar que haya al menos un parametro
+if [ $# -eq 0 ]; then
+    echo "Error: No se proporciono ningun argumento, se necesitan al menos el archivo que contenga los usuarios" 2>&1
+    exit 1
+fi
