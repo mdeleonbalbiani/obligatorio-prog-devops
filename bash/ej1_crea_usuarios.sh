11c1f72580e64ee1a67fdc7f80a7876c974de5e2
mdeleonbalbiani <martinadeleonbalbiani@gmail.com>
2025-11-12 22:22:35 -0300

    Validación de 1er  paramentro

--- bash/ej1_crea_usuarios.sh
+++ bash/ej1_crea_usuarios.sh
@@ -10,3 +10,15 @@ if [ $# -eq 0 ]; then
     echo "Error: No se proporciono ningun argumento, se necesitan al menos el archivo que contenga los usuarios" 2>&1
     exit 1
 fi
+
+#Validación primer parametro
+if [ $1 = "-i" ]; then
+    modo_info=true
+else
+    if [ $1 = "-c" ]; then
+        password=$2
+    else
+        archivo=$1
+    fi 
+fi
+
